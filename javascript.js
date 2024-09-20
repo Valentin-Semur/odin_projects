780988125bc96c7f93ff7f13999ef796b3353ab4
Valentin-Semur <valentin.semur@gmail.com>
2024-09-20 11:57:21 +0200

    feat: add basic calculator functions

---
+++ javascript.js
@@ -0,0 +1,37 @@
+
+// Basic math functions
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return substract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
+
