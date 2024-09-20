db7d5f14758ba1f82cf8680415fa4b42641d7789
Valentin-Semur <valentin.semur@gmail.com>
2024-09-20 15:05:38 +0200

    feat: implement full calculator logic

--- javascript.js
+++ javascript.js
@@ -1,26 +1,25 @@
 
 // Basic math functions
 function add(a, b) {
-    return a + b
+    return +a + +b;
 }
 
 function substract(a, b) {
-    return a - b
+    return a - b;
 }
 
 function multiply(a, b) {
-    return a * b
+    return a * b;
 }
 
 function divide(a, b) {
-    return a / b
+    return a / b;
 }
 
-
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
 let operator;
-
+const display = document.querySelector("#results");
 
 function operate(a, b, operator) {
     switch (operator) {
@@ -35,3 +34,67 @@ function operate(a, b, operator) {
     }
 }
 
+function setResult() {
+    if (firstNumber === "" || secondNumber === "" || operator === undefined) {
+        return;
+    }
+    if (+secondNumber == 0 && operator === "/") {
+        updateDisplay("nooope");
+        return;
+    }
+    result = operate(firstNumber, secondNumber, operator);
+    updateDisplay(result);
+    firstNumber = result;
+    secondNumber = "";
+    operator = undefined;
+}
+
+function addNumber(e) {
+    if (e.currentTarget.textContent == "." && display.textContent.includes(".")) {
+        return;
+    }
+    if (operator === undefined) {
+        firstNumber += e.currentTarget.textContent;
+        updateDisplay(firstNumber);
+    } else {
+        secondNumber += e.currentTarget.textContent;
+        updateDisplay(secondNumber);
+    }
+}
+
+function addOperator(e) {
+    if (operator === undefined) {
+        operator = e.currentTarget.textContent;
+    } else {
+        setResult();
+        operator = e.currentTarget.textContent;
+    }
+}
+
+function updateDisplay(text) {
+    display.textContent = text;
+}
+
+function resetCalculator() {
+    updateDisplay("result");
+    firstNumber = "";
+    secondNumber = "";
+    operator = undefined;
+}
+
+// Create the event listeners
+const numberButtons = document.querySelectorAll(".number, #key-dot");
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener("click", addNumber);
+}
+
+const ac = document.querySelector("#keyAC");
+ac.addEventListener("click", resetCalculator);
+
+const operatorButtons = document.querySelectorAll(".operator");
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener("click", addOperator);
+}
+
+const equalButton = document.querySelector("#key-equal");
+equalButton.addEventListener("click", setResult);
