7d97094a374c0ddd9278e98f25ef4de8463830ca
Valentin-Semur <valentin.semur@gmail.com>
2024-10-17 10:13:56 +0200

    feat: add isaac characters

--- 09_tic-tac-toe/script.js
+++ 09_tic-tac-toe/script.js
@@ -34,22 +34,20 @@ function Cell() {
     return { changeValue, getValue };
 }
 
-function Player(name, marker) {
+function Player(name, marker, character) {
     let score = 0;
 
     const addScore = () => score++;
     const getScore = () => score;
 
-    return { name, marker, addScore, getScore };
+    return { name, marker, character, addScore, getScore };
 }
 
 const GameController = (function () {
     const players = [];
     const scoreLimit = 3;
 
-    const initPlayers = () => {
-        const player1 = Player("Valentin", 1);
-        const player2 = Player("Magali", 2);
+    const initPlayers = (player1, player2) => {
         players.push(player1);
         players.push(player2);
     }
@@ -138,14 +136,17 @@ const Utils = (function () {
 
 
 const DisplayController = (function () {
-    const gameboard_container = document.querySelector(".gameboard");
+    let player1;
+    let player2;
+
 
     const initGameboard = () => {
+        const gameboard_container = document.querySelector(".gameboard");
+
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 const cell = document.createElement("div");
                 cell.id = `${i},${j}`;
-                cell.textContent = "X"
                 cell.addEventListener("click", logClick);
 
                 gameboard_container.appendChild(cell)
@@ -154,72 +155,135 @@ const DisplayController = (function () {
     }
 
     function logClick() {
-        console.log(this.id);
+        const character = Match.getActivePlayer().character;
+        const itemImage = document.createElement("img");
+        itemImage.src = `img/characters/item-${character}.webp`;
+        this.appendChild(itemImage);
+
+        const coordinates = this.id.split(",");
+        Match.playRound(coordinates[0], coordinates[1]);
+        highlightCurrentPlayer();
+
+        console.log(coordinates);
+        console.log(character);
+    }
+
+    function highlightCurrentPlayer() {
+        const currentPlayer = `player${Match.getActivePlayer().marker}`;
+        const currentPlayerImage = document.querySelector(`#${currentPlayer}>img`);
+        currentPlayerImage.classList.toggle()
+
+        console.log(currentPlayerDiv)
+    }
+
+    const initCharacterSelection = () => {
+
+        const isaacCharacters = [
+            "isaac",
+            "magdalene",
+            "cain",
+            "judas",
+            "bluebaby",
+            "eve",
+            "samson",
+            "azazel",
+            "lazarus",
+            "eden",
+            "lost",
+            "lilith",
+            "keeper",
+            "apollyon",
+            "forgotten",
+            "darkjudas"
+        ];
+
+        for (const player of ["player1", "player2"]) {
+            const playerArea = document.querySelector(`#${player}`);
+            playerArea.innerHTML = "";
+
+            // Name picker
+            const formDiv = document.createElement("div");
+            formDiv.classList.add("player-name");
+            const formTitle = document.createElement("label");
+            formTitle.textContent = "Name";
+            formTitle.setAttribute("for", player);
+            const formInput = document.createElement("input");
+            formInput.setAttribute("type", "text");
+            formInput.setAttribute("name", player);
+            formInput.value = `Nameof${player}`; // TEST 
+            formDiv.appendChild(formTitle);
+            formDiv.appendChild(formInput);
+            playerArea.appendChild(formDiv);
+
+            // Character chooser
+            const characterChoiceTitle = document.createElement("div");
+            characterChoiceTitle.classList.add("title-score");
+            characterChoiceTitle.textContent = "Pick a character";
+            playerArea.appendChild(characterChoiceTitle);
+
+            const character = document.createElement("div");
+            character.classList.add("characters");
+            for (let i = 0; i < 16; i++) {
+                const charac = document.createElement("img");
+                charac.src = `img/characters/${isaacCharacters[i]}.webp`;
+                charac.id = isaacCharacters[i];
+                charac.classList.add("chooser");
+                charac.addEventListener("click", selectPlayer);
+                character.appendChild(charac);
+            };
+            playerArea.appendChild(character);
+        }
+    }
+
+    function selectPlayer() {
+        const selectedCharacter = this.id;
+        const playerNumber = this.parentElement.parentElement.id;
+        const playerName = document.querySelector(`[name=${playerNumber}]`).value
+
+        if (playerName === "") {
+            return;
+        };
+
+        if (playerNumber === "player1") {
+            player1 = Player(playerName, 1, selectedCharacter);
+        } else {
+            player2 = Player(playerName, 2, selectedCharacter);
+        };
+
+        initPlayerScreen(playerNumber, playerName, selectedCharacter);
+
+        if ((player1 != undefined) && (player2 != undefined)) {
+            GameController.initPlayers(player1, player2);
+            DisplayController.initGameboard();
+            Match.initMatch();
+            highlightCurrentPlayer();
+        }
     }
 
 
+    const initPlayerScreen = (player, name, character) => {
+        const playerArea = document.querySelector(`#${player}`);
+        playerArea.innerHTML = "";
+
+        const playerName = document.createElement("div");
+        playerName.classList.add("player-name");
+        playerName.textContent = name;
+        playerArea.appendChild(playerName);
+
+        const score = document.createElement("div");
+        score.classList.add("title-score");
+        score.textContent = "Score: 0"
+        playerArea.appendChild(score);
+
+        const playerCharacter = document.createElement("img");
+        playerCharacter.classList.add("character");
+        playerCharacter.src = `img/characters/${character}.webp`;
+        playerArea.appendChild(playerCharacter);
+    }
 
 
-    return { initGameboard };
+    return { initGameboard, initCharacterSelection };
 })();
 
-DisplayController.initGameboard();
 
-function playTestRows() {
-    GameController.initPlayers();
-    Match.initMatch();
-    Match.playRound(0, 0)
-    Match.playRound(1, 0)
-    Match.playRound(0, 1)
-    Match.playRound(1, 1)
-    Match.playRound(2, 2)
-    Match.playRound(1, 2)
-}
-
-function playTestColumns() {
-    GameController.initPlayers();
-    Match.initMatch();
-    Match.playRound(0, 0)
-    Match.playRound(0, 1)
-    Match.playRound(1, 0)
-    Match.playRound(1, 1)
-    Match.playRound(2, 0)
-}
-
-function playTestDiagonals() {
-    GameController.initPlayers();
-    Match.initMatch();
-    Match.playRound(0, 0)
-    Match.playRound(0, 1)
-    Match.playRound(1, 1)
-    Match.playRound(1, 0)
-    Match.playRound(2, 2)
-}
-
-function playTestRC() {
-    GameController.initPlayers();
-    Match.initMatch();
-    Match.playRound(0, 1)
-    Match.playRound(1, 1)
-    Match.playRound(0, 2)
-    Match.playRound(1, 2)
-    Match.playRound(1, 0) 
-    Match.playRound(2, 1)
-    Match.playRound(2, 0)
-    Match.playRound(2, 2)
-    Match.playRound(0, 0)
-}
-
-function playTestNull() {
-    GameController.initPlayers();
-    Match.initMatch();
-    Match.playRound(0, 0)
-    Match.playRound(0, 1)
-    Match.playRound(0, 2)
-    Match.playRound(1, 1) 
-    Match.playRound(1, 0)
-    Match.playRound(1, 2)
-    Match.playRound(2, 1)
-    Match.playRound(2, 0)
-    Match.playRound(2, 2)
-}
+DisplayController.initCharacterSelection();
