3540dad7b9e7013df6dd0b9ce434f0daaf68eb25
Valentin Semur <valentin@Valentins-Mac-mini.local>
2025-05-07 16:53:06 +0200

    feat: create the html tags in js

--- 12_weather-app/src/index.js
+++ 12_weather-app/src/index.js
@@ -1,9 +1,10 @@
 import "./styles.css";
 import config from "../config.js";
 import { convertToCelsius, convertToKmH } from "./utils.js";
+import searchIcon from "./assets/icons/search.svg";
 
 // Load icons
-const icons = require.context("./assets/icons", false, /\.svg$/);
+const icons = require.context("./assets/weather-icons", false, /\.svg$/);
 const weatherIcons = {};
 icons.keys().forEach(key => {
     const iconName = key.replace("./", "").replace(/\.svg$/, '');
@@ -13,7 +14,7 @@ icons.keys().forEach(key => {
 
 const apiKey = config.apiKey;
 
-
+// Fetch weather data from the API
 const fetchWeather = async (city) => {
     const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`;
     const response = await fetch(apiUrl);
@@ -22,6 +23,7 @@ const fetchWeather = async (city) => {
     return data;
 }
 
+// Display weather data on the page (update the DOM)
 const displayWeatherData = (weatherData) => {
     document.getElementById("weather-description").textContent = weatherData.description;
     document.getElementById("weather-location").textContent = weatherData.location;
@@ -36,6 +38,7 @@ const displayWeatherData = (weatherData) => {
     document.getElementById("weather-icon").innerHTML = `<img src="${iconSrc}" alt="${weatherData.icon}">`;
 }
 
+// Extract weather data from the API response
 const extractWeatherData = (data) => {
     const { currentConditions, resolvedAddress } = data;
     const { temp, conditions, datetime, precipprob, windspeed, humidity, icon } = currentConditions;
@@ -54,6 +57,7 @@ const extractWeatherData = (data) => {
     return weatherData;
 }
 
+// Update weather data
 const updateWeatherData = async () => {
     const defaultCity = "Paris";
     const city = document.getElementById("city-input").value || defaultCity;
@@ -63,6 +67,7 @@ const updateWeatherData = async () => {
     displayWeatherData(weatherData);
 }
 
+// Event listeners
 const getWeatherButton = document.getElementById("get-weather");
 getWeatherButton.addEventListener("click", updateWeatherData); 
 
@@ -73,5 +78,46 @@ cityInput.addEventListener("keydown", (event) => {
     }
 });
 
+document.getElementById("get-weather").innerHTML = `<img src="${searchIcon}" alt="search">`;
+console.log(document.getElementById("get-weather"));
+
+const createElement = (tag, parent, { id = null, classes = []}) => {
+    const element = document.createElement(tag);
+    if (id) element.id = id;
+    if (classes.length > 0) classes.forEach(className => element.classList.add(className));
+    parent.appendChild(element);
+    return element;
+}
+
+const container = document.getElementById("container");
+console.log(container);
+const weatherContainer = createElement("div", container, { id: "weather-container" });
+
+const weatherDivIds = [
+    "weather-description",
+    "weather-location",
+    "weather-short-description",
+    "weather-datetime",
+    "weather-precipitation",
+    "weather-humidity",
+    "weather-wind",
+]
+
+weatherDivIds.map(id => createElement("div", weatherContainer, { id: id }));
+
+const createWeatherIconTemperature = () => {
+    const weatherIconTemperature = createElement("div", weatherContainer, { id: "weather-icon-temperature" });
+    const weatherIcon = createElement("div", weatherIconTemperature, { id: "weather-icon" });
+    const weatherTemperature = createElement("div", weatherIconTemperature, { id: "weather-temperature" });
+    console.log(weatherIconTemperature);
+    return weatherIconTemperature;
+}
+
+createWeatherIconTemperature();
+
+
+
 // Update weather data on page load to Paris
 updateWeatherData();
+console.log(weatherContainer);
+
