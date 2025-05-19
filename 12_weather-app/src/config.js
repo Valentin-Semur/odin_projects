aaee8b659d84504b4688c5a51fa9f4962fd4814f
Valentin Semur <valentin@Valentins-Mac-mini.local>
2025-05-19 09:03:24 +0200

    fix: update gitignore to add config and dist

---
+++ 12_weather-app/src/config.js
@@ -0,0 +1,8 @@
+// config.js
+const config = {
+    apiKey: 'ZWK488AQ8SLJ8SMLFGQ4QKW4C',
+    apiUrl: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+    defaultCity: 'Paris',
+};
+
+export default config;
