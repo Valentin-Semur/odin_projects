53a01d5d128cc07245482e049889819abcd60dce
Valentin Semur <valentin@Valentins-Mac-mini.local>
2025-05-22 17:33:40 +0200

    feat: create the Cell factory

--- 13_knights-travails/src/index.js
+++ 13_knights-travails/src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+
+const Cell = (x, y, distance = 0, predecessor = null) => {
+    return { x, y, distance, predecessor }
+}
+
+const knightMoves = () => {
+    
+}
+
+
+
+
+
+/* 
+const newCell = Cell(1, 2, 2, Cell(2,3));
+console.log(newCell)
+*/
